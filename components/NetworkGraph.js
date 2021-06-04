5a4b38260439b9d1b8758c311df3665c273bab1d
Nick Bohmann <nbohmann@uw.edu>
2021-06-04 14:09:35 -0700

    updated network graph component

--- components/NetworkGraph.js
+++ components/NetworkGraph.js
@@ -1,45 +1,48 @@
 const React = require('react');
 const D3Component = require('idyll-d3-component');
 const d3 = require('d3');
-const { svg } = require('d3');
+const { canvas } = require('d3');
 
 class NetworkGraph extends D3Component {
 
   initialize(node,props) {
-    const svg = d3.select(node).append('svg')
+    var data = props.data
+    var canvas = d3.select(node).append('canvas')
         .attr("viewBox", [0, 0, 1200, 1200]);
-    d3.json("https://raw.githubusercontent.com/cse412-21sp/genetic-genealogy-network/main/data/triangulated_segments.json", function(error,data){
-    const links = data.links.map(d => Object.create(d));
-    const nodes = data.nodes.map(d => Object.create(d));
-  
-    const width = 1200
-    const height = 1200 
+    var links = data.links.map(d => Object.create(d));
 
-    const simulation = d3.forceSimulation(nodes)
-        .force("link", d3.forceLink(links).id(d => d.id))
+    var nodes = data.nodes.map(d => Object.create(d));
+
+    var width = 1200
+
+    var height = 1200 
+
+    var scale = d3.scaleLog()
+    .domain([1,20])
+    .range([2,4,6,8,10])
+
+    var simulation = d3.forceSimulation(samples)
+        .force("link", d3.forceLink(link).id(d => d.id))
         .force("charge", d3.forceManyBody())
         .force("x", d3.forceX(width / 2))
         .force("y", d3.forceY(height / 2))    
-  
-    const link = svg.append("g")
+
+    const link = canvas.append("g")
         .attr("stroke-opacity", 0.6)
          .selectAll("line")
       .data(links)
       .join("line")
         .attr("stroke-width", d => Math.sqrt(d.value))
-        .attr("stroke", color);
+        .attr("stroke", 'gray');
   
-    const samples = svg.append("g")
+    const samples = canvas.append("g")
         .attr("stroke", "#fff")
         .attr("stroke-width", 1.5)
       .selectAll("circle")
       .data(nodes)
       .join("circle")
-        .attr("r", size)
-        .attr("fill", 'steelblue')
-        .call(drag(simulation));
-  
-
+        .attr("r", d => scale(d.overlap))
+        .attr("fill", 'steelblue');
 
     simulation.on("tick", () => {
       link
@@ -51,12 +54,14 @@ class NetworkGraph extends D3Component {
       samples
           .attr("cx", d => d.x)
           .attr("cy", d => d.y);
-    });
-  
-    invalidation.then(() => simulation.stop());
-  
-    return svg.node();
-  })
+
+
+    
+    }
+  );
+
+    return canvas.node();
+
   }
 }
 module.exports = NetworkGraph;
